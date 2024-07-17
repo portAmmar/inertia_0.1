bff52c5c7351293abbd353b2a00542a1deaf0c88
ammar <ammar@fmail.com>
2024-07-17 18:17:23 +0300

    01

--- database/migrations/2024_07_17_150138_create_tasks_table.php
+++ database/migrations/2024_07_17_150138_create_tasks_table.php
@@ -18,7 +18,11 @@ return new class extends Migration
             $table->string('img_path')->nullable();
             $table->string('status');
             $table->string('priority');
-
+            $table->timestamp('due_date')->nullable();
+            $table->foreignId('assigned_user_id')->constrained('users');
+            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('updated_by')->constrained('users');
+            $table->foreignId('project_id')->constrained('projects');
             $table->timestamps();
         });
     }
